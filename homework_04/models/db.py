f9347c047ff5d1dfb7fcc015c657465e51a82ee3
artyom <artyomevseev@gmail.com>
2024-03-27 18:06:18 +0300

    Add solution for homework_04_v2

--- homework_04/models/db.py
+++ homework_04/models/db.py
@@ -2,8 +2,8 @@ from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 import os
 
 
-PG_CONN_URI =  "postgresql+asyncpg://postgres:admin@localhost:5432/postgres" # локальная бд для тестирования
-# PG_CONN_URI = os.environ.get("SQLALCHEMY_PG_CONN_URI") or "postgresql+asyncpg://postgres:password@localhost/postgres"
+#PG_CONN_URI =  "postgresql+asyncpg://postgres:admin@localhost:5432/postgres" # локальная бд для тестирования
+PG_CONN_URI = os.environ.get("SQLALCHEMY_PG_CONN_URI") or "postgresql+asyncpg://postgres:password@localhost/postgres"
 
 engine = create_async_engine(PG_CONN_URI)
 
